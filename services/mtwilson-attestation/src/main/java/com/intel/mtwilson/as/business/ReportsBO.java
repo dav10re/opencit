2196efd7b7e8534035de30454638bf913170bbe2
dav10re <Davidre10@MacBook-di-Davide.local>
2018-03-02 17:48:09 +0100

    Trying to show the SHA1 value of pcr 10:
    * HostTrustBO.java: added an if clause to avoid to reject PCR 10

--- services/mtwilson-attestation/src/main/java/com/intel/mtwilson/as/business/ReportsBO.java
+++ services/mtwilson-attestation/src/main/java/com/intel/mtwilson/as/business/ReportsBO.java
@@ -471,6 +471,15 @@ public class ReportsBO {
 
         if (pcrManifestCollection != null) {
             for (TblPcrManifest pcrManifest : pcrManifestCollection) {
+                
+                //--------- Added by dav10re ---------
+                /* I insert this if clause in such way that I can return the pcr 10 value, otherwise it will be rejected. In fact when getPcrBank method is called, this may return a SHA256 string and the pcr 10 won't pass the if clause because it is a SHA1 value.  */
+                
+                if (pcrMAnifest.getName().equals("10"))
+                    return pcrManifest;
+                
+                //-----------------------------------
+                
                 if (pcrManifest.getName().equals(manifestName) && pcrManifest.getPcrBank().equals(tblHosts.getPcrBank())) {
                     return pcrManifest;
                 }
